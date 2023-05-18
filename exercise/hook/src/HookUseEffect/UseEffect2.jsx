a6466cc226831f70f0eea1a5adea9fbcde454ed2
jisooyoon <yoonmallang@gmail.com>
2023-05-18 19:54:37 +0900

    2023-05-15 useEffect, clean-up

--- exercise/hook/src/HookUseEffect/UseEffect2.jsx
+++ exercise/hook/src/HookUseEffect/UseEffect2.jsx
@@ -6,8 +6,20 @@ export default function Time() {
   const [min, setMin] = useState(today.getMinutes());
   const [sec, setSec] = useState(today.getSeconds());
 
-  console.log("렌더링이 됩니다..!"); // 렌더링이 잘 되는지 확인용 꼭 넣고 진행하기
+  // console.log("렌더링이 됩니다..!"); // 렌더링이 잘 되는지 확인용 꼭 넣고 진행하기
 
+  // 1초마다 setState하는 인터벌이 쌓인다
+  // -> 보이지 않는 곳에서 부하가 일어나고 있다
+  // setInterval(() => {
+  //   console.log("화면 새로고침");
+  //   const today = new Date();
+  //   setToday(today);
+  //   setHour(today.getHours());
+  //   setMin(today.getMinutes());
+  //   setSec(today.getSeconds());
+  // }, 1000);
+
+  // 처음에 한 번은 무조건 실행하고 그 다음부터는 [] 안에 있는 값을 기준으로 실행한다.
   useEffect(() => {
     const interval = setInterval(() => {
       const today = new Date();
